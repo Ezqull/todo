6af91ec57fcda7cff38d1b13a35bea81405310a3
Michal Salamon <michal.salamon@SBPL99HK3DL.local>
2023-11-02 09:47:32 +0100

    base project structure

---
+++ src/main/java/project/app/nocando/NocandoApplication.java
@@ -0,0 +1,13 @@
+package project.app.nocando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NocandoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NocandoApplication.class, args);
+    }
+
+}
